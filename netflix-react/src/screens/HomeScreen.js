84684e09d21edc22c44d8b4c85144111e5061766
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-02 12:59:05 +0530

    ADD: Nav bar

---
+++ netflix-react/src/screens/HomeScreen.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+function HomeScreen() {
+    return (
+        <div className='homeScreen'> 
+             {/* nav */}
+             <Nav />
+           
+
+             {/* banner */}
+
+
+             {/* row */}
+        </div>
+    )
+}
+
+export default HomeScreen
