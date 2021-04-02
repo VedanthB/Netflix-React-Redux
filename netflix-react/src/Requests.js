9d952d1230312f9319214cdd74fe62c0d5871d4f
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-02 14:34:59 +0530

    ADD: TMBD API requests and implemented the result to change the banner on every refresh

---
+++ netflix-react/src/Requests.js
@@ -0,0 +1,13 @@
+
+const API_KEY = '408438c150c60afbb08def435ca42cb1';
+
+const requests = {
+    fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`, 
+    fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`, 
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+}
+
+export default requests;
--- netflix-react/src/components/Banner.js
+++ netflix-react/src/components/Banner.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import axios from '../axios';
+import React, { useEffect, useState } from 'react'
+import requests from '../Requests';
 import './Banner.css'
 
+
+
+
 function Banner() {
-    
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+       async function fetchData() {
+           const request = await axios.get(requests.fetchTrending);
+           setMovie(
+               request.data.results[
+                   Math.floor(Math.random() * request.data.results.length - 1)
+               ]
+           )
+           return request;
+       }
+       fetchData();
+
+    }, [])
+
+    console.log(movie);
+
     function truncate(string, n) {
         return string?.length > n ? string.substr(0, n-1) + '....' : string;
      } 
@@ -12,23 +34,23 @@ function Banner() {
         className='banner'
         style={{
             backgroundSize: 'cover',
-            backgroundImage: `url(https://torranceca.files.wordpress.com/2019/10/netflix.jpg)`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: 'center center',
         }}
        > 
        <div className='banner__contents' > 
-          <h1 className='banner__title' >Movie Name</h1>
+          <h1 className='banner__title' >
+               {movie?.title || movie?.name || movie?.original_name }
+           </h1>
+
              <div className='banner__buttons' >
                  <button className='banner__button'>Play</button>
                  <button className='banner__button'>My List</button>
             </div>
-            <h1 className='banner__description'>
-                {truncate(`This is a tescriptionT
-                           his is a test dhis is a test d
-                           his is a test d
-                           his is a test dhis is a test d
-                           his is a test dvhis is a test description`, 150)}
-                </h1>
+
+              <h1 className='banner__description'>
+                  {truncate(movie?.overview, 150)}
+              </h1>
        </div>
 
        <div className='banner--fadeBottom'></div>
