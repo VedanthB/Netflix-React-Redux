cbd3c0cd5c34cc5a415fa35101f8bba10d1ba624
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-02 12:08:20 +0530

    initial commit

---
+++ netflix-react/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCGEmM5fBP-IEBOLfMIxlKGGyrdC-FH5-s",
+    authDomain: "netflix-react-e8df4.firebaseapp.com",
+    projectId: "netflix-react-e8df4",
+    storageBucket: "netflix-react-e8df4.appspot.com",
+    messagingSenderId: "66417759020",
+    appId: "1:66417759020:web:e060dfe6b9b1a5b3be9289",
+    measurementId: "G-NMX3QKKCHC"
+  };
