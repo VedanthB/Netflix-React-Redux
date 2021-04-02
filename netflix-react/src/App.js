965eb0ee6945ed31010431703ef49c1388b5bc6f
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-02 16:44:35 +0530

    ADD: login with google

--- netflix-react/src/App.js
+++ netflix-react/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {BrowserRouter as Router, Switch, Route,} from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, selectUser } from './features/userSlice';
+import { auth } from './firebase';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+   useEffect(() => {
+      auth.onAuthStateChanged((user) => {
+        if (user)  {
+          // the user is logged in
+           dispatch(
+             login({
+               displayName: user.displayName,
+               email: user.email,
+               photoUrl: user.photoURL,
+           })
+           )
+        }
+
+      })
+   }, [dispatch])
 
   return (
     <div className="app">
