eb100c8377745d313c5929d9143dcab9e986c26d
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-04-02 15:06:24 +0530

    ADD: sign up Screen

--- netflix-react/src/screens/SignupScreen.js
+++ netflix-react/src/screens/SignupScreen.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import './SignupScreen.css'
 
 function SignupScreen() {
     return (
-        <div>
-            
+        <div className='signupScreen' >
+              <form>
+                 <h1>Sign In</h1>
+
+                 <input placeholder='Email' type='email' />
+                 <input placeholder='Password' type='password' />
+
+                 <button type='submit'>Sign In</button>
+
+                 <h4>
+                      <span className='signupScreen__gray' >New to Netflix? </span>
+                      <span className='signupScreen__link' >Sign Up Now. </span>
+                  </h4>
+             </form>
         </div>
     )
 }
